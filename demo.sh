4fa4fbb9d71a1e2b7da41e013797feed4a0c8a2f
Brian Li <brianlee0415@gmail.com>
2024-06-08 22:45:11 +0800

    feat: add demo code and result

---
+++ demo.sh
@@ -0,0 +1,9 @@
+input=demo/original.jpg
+
+python oil_painting.py --input $input --output demo/oil_painting.jpg
+python water_color.py --input $input --output demo/water_color.jpg
+python stained_glass.py --input $input --n_points=10000 --output demo/stained_glass.jpg
+
+montage demo/original.jpg demo/stained_glass.jpg \
+        demo/oil_painting.jpg demo/water_color.jpg \
+        -tile 2x2 -geometry +0+0 "demo/grid.jpg"
