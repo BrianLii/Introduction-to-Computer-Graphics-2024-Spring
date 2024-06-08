47b84cb0ad6ed17c42339b92c2dee8a345827a66
Brian Li <brianlee0415@gmail.com>
2024-06-09 00:24:12 +0800

    fix: update make_report.sh

--- make_report.sh
+++ make_report.sh
@@ -1,6 +1,7 @@
+bash demo.sh
 bash experiment_oil_painting.sh
 bash experiment_water_color.sh
 bash experiment_stained_glass.sh
 
-rm -rf report/resources/{demo, experiment}
+rm -rf report/resources/{demo,experiment}
 cp -r demo experiment report/resources
